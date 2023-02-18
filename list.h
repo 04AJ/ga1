b585666d544a4879567777528bb9229039d185f2
Alan Johnson <alanjcd2004@gmail.com>
2023-02-18 16:38:31 -0600

    updated function definitions

--- list.h
+++ list.h
@@ -1,24 +1,30 @@
 #include <iostream>
 using namespace std;
 
-struct node {
-    node* next;
+struct node
+{
+    node *next;
     int num;
-
+    bool duplicate;
     node();
-    node(int n){num=n; next=nullptr;}
+    node(int n)
+    {
+        num = n;
+        next = nullptr;
+    }
 };
 
-
-class list{
+class list
+{
 private:
-	node* head;
+    node *head;
+
 public:
-	//ALL FUNCTIONS ARE RECURSIVE
-    list();
-	void print(node* head, ofstream &output, bool answer);
-	void addtoend();
-	bool duplicate();
-	
-	
+    list() { head = nullptr; };
+
+    // ALL FUNCTIONS ARE RECURSIVE
+    void addtoend();
+    void msort();
+    // for each node assign node->duplicate value and delete them(?)
+    void duplicate();
 };
--- main.cpp
+++ main.cpp
@@ -3,20 +3,21 @@
 #include <array>
 #include <string>
 #include "ArgumentManager.h"
+#include "list.h"
 using namespace std;
 
+int decode(string input);
+void print(node *head, ofstream &output);
 
-
-
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     ArgumentManager an(argc, argv);
     const string input = an.get("input");
     const string command = an.get("command");
     const string output = an.get("output");
-    //ifstream inputfile(input);
+    // ifstream inputfile(input);
     ofstream outfile(output);
 
-    //delete when done
+    // delete when done
     ifstream inputfile("input2.txt");
-
 }
