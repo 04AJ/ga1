af6803287d6c14e958c775fded284761abfcaeaa
lindolfo1 <cuentas@feptec.com>
2023-02-18 14:15:59 -0600

    modified:   list.h

--- list.h
+++ list.h
@@ -1,8 +1,24 @@
-struct node {
+#include <iostream>
+using namespace std;
 
+struct node {
+    node* next;
+    int num;
+
+    node();
+    node(int n){num=n; next=nullptr;}
 };
+
 
 class list{
 private:
-    node *head;
+	node* head;
+public:
+	//ALL FUNCTIONS ARE RECURSIVE
+    list();
+	void print(node* head, ofstream &output, bool answer);
+	void addtoend();
+	bool duplicate();
+	
+	
 };
