b06da2aec7fc049bce7d3c4df259850333d4e621
lindolfo1 <cuentas@feptec.com>
2023-02-25 13:11:40 -0600

    new file:   .vscode/launch.json 	new file:   .vscode/tasks.json 	modified:   list.cpp 	modified:   list.h 	new file:   main 	modified:   main.cpp

---
+++ list.cpp
@@ -0,0 +1,26 @@
+// #include "list.h"
+
+
+// bool list::push(char c) {
+//     node *tmp = new node();
+//     tmp->c = c;
+//     tmp->next = head;
+//     head = tmp;
+//     return true;
+// }
+
+
+// char list::pop() {
+//     if (head != nullptr) {
+//         node *tmp = head;
+//         head = head->next;
+//         char c = tmp->c;
+//         delete tmp;
+//         return c;
+//     }
+//     return ' ';
+// }
+
+// node* list::peek(){
+//     return head;
+// }
--- list.h
+++ list.h
@@ -4,10 +4,27 @@ using namespace std;
 struct node {
     node* next;
     int num;
+    char c;
 
-    node();
-    node(int n){num=n; next=nullptr;}
+    node() {};
+    node(int n){num=n; next=nullptr; c=' ';}
 };
+
+
+// class list{
+// private:
+// 	node* head;
+// public:
+// 	//ALL FUNCTIONS ARE RECURSIVE
+//  list();
+// 	void print(node* head, ofstream &output, bool answer);
+// 	void addtoend();
+// 	bool duplicate();
+// 	bool push(char);
+//  char pop();
+// 	node* peek();
+// };
+
 
 
 class list{
@@ -15,10 +32,35 @@ private:
 	node* head;
 public:
 	//ALL FUNCTIONS ARE RECURSIVE
-    list();
+    list() {head = nullptr;};
 	void print(node* head, ofstream &output, bool answer);
 	void addtoend();
 	bool duplicate();
-	
-	
+
+
+    bool push(char c) {
+        node *tmp = new node();
+        tmp->c = c;
+        tmp->next = head;
+        head = tmp;
+        return true;
+    }
+
+
+    char pop() {
+        if (head != nullptr) {
+            node *tmp = head;
+            head = head->next;
+            char c = tmp->c;
+            delete tmp;
+            return c;
+        }
+        return ' ';
+    }
+
+    node* peek(){
+        return head;
+    }
+
+    bool isEmpty () {return head == nullptr;}
 };
