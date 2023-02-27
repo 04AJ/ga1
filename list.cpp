4b17126f6b2d587a2d9faab1aa8c996e2002c234
Hernan Cabrera <91387687+GearUltra@users.noreply.github.com>
2023-02-26 19:33:26 -0600

    Update list.cpp

---
+++ list.cpp
@@ -0,0 +1,29 @@
+#include "list.h"
+node *list::newNode(string data){
+      node *new_node = new node;
+      new_node->num = data;
+      new_node->next = nullptr;
+      return new_node;
+    }
+
+    node *list::addtoend(node* head,string bar){
+      
+    if (head == nullptr) 
+      return newNode(bar);
+    else
+      head->next=addtoend(head->next, bar);
+      return head;
+    }
+
+  
+
+    void list::print(node *head ){
+       if(head == nullptr)
+         //cout<<"yep"<<endl;
+         return;
+       
+       cout << head->num <<endl;
+         
+       print(head->next);
+       
+    }
